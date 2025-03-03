633225ae101fa0df79a495a400724f34e8e9c27c
hana Jouini <hana.juini@gmail.com>
2025-03-03 22:40:50 +0100

    firt test case

--- cypress/e2e/hana.cy.js
+++
@@ -1,8 +0,0 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-    /* ==== Generated with Cypress Studio ==== */
-    cy.visit('www.google.com');
-    /* ==== End Cypress Studio ==== */
-  })
-})
---
+++ cypress/e2e/mocha.cy.js
@@ -0,0 +1,5 @@
+/// <reference types = "cypress" />
+
+it("log something in cypress", function(){
+    cy.log("I am inside a test case !")
+});
