7945e23e7aec2b67e34af448e0623c70eceb9980
hana Jouini <hana.juini@gmail.com>
2025-03-03 21:02:54 +0100

    first

---
+++ cypress/e2e/hana.cy.js
@@ -0,0 +1,8 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://example.cypress.io')
+    /* ==== Generated with Cypress Studio ==== */
+    cy.visit('www.google.com');
+    /* ==== End Cypress Studio ==== */
+  })
+})
